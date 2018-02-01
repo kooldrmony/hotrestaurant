0a68c01c2bf23fdf0d3c42f4c5bceefd5f0cc9b6
kooldrmony <derrickreyes.23@gmail.com>
2018-01-31 20:36:54 -0700

    addding js file

---
+++ restaurant.js
@@ -0,0 +1,75 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+var reservations = [];
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Get all characters
+app.get("/all", function(req, res) {
+  res.json(reservations);
+});
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/:reservations?", function(req, res) {
+  var time = req.params.reservations;
+
+  if (time) {
+    console.log(time);
+
+    for (var i = 0; i < reservations.length; i++) {
+      if (time === reservations[i].routeName) {
+        return res.json(reservations[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(reservations);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newreservation = req.body;
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newreservation);
+
+  reservations.push(newreservation);
+
+  res.json(newreservation);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
